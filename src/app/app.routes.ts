9539d00f995c63ed8dfc9201d19d7956274e4863
krzysztof.kobus <krzysztof.kobus@tutamail.com>
2026-07-15 18:18:24 +0200

    update routes - eager dashboard + lazy machine-details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+  },
+  {
+    path: 'machine/:id',
+    loadComponent: () =>
+      import('./features/machine-details/machine-details.component').then(m => m.MachineDetailsComponent),
+  },
+];
