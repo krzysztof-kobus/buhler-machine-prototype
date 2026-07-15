374a54459cc7f7ec8ec65a758c14898e73eb16bf
krzysztof.kobus <krzysztof.kobus@tutamail.com>
2026-07-15 18:17:35 +0200

    introduce basic error handler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { GlobalErrorHandler } from './core/handlers/global-error.handler';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: ErrorHandler, useClass: GlobalErrorHandler },
+  ],
 };
