992cd0cf9f27c633a0eb594cd8fb2362cec436a6
krzysztof.kobus <krzysztof.kobus@tutamail.com>
2026-07-15 18:12:44 +0200

    create API service

---
+++ src/app/core/services/machines-api.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { retry } from 'rxjs';
+import { Machine } from '../models/machine.model';
+
+@Injectable({ providedIn: 'root' })
+export class MachinesAPIService {
+  private readonly http = inject(HttpClient);
+
+  readonly machinesResource = rxResource({
+    loader: () =>
+      this.http.get<Machine[]>('/assets/data/machines.json').pipe(
+        retry({ count: 3, delay: 1000 })
+      )
+  });
+
+  readonly machines = this.machinesResource.value;
+  readonly isLoading = this.machinesResource.isLoading;
+  readonly error = this.machinesResource.error;
+}
